1524c84efff85bfd32313837b5b2246f414e6e8c
Andrew <atklenner@gmail.com>
2022-01-24 10:59:45 -0800

    add answers

---
+++ easy/darts.js
@@ -0,0 +1,7 @@
+export const score = (x, y) => {
+  let distance = Math.sqrt(x * x + y * y);
+  if (distance > 10) return 0;
+  if (distance > 5) return 1;
+  if (distance > 1) return 5;
+  return 10;
+};
