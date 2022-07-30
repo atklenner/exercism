8bb7da840761044fa115cc2df4c0ddf24906d06b
Andrew Klenner <atklenner@gmail.com>
2022-07-30 13:04:22 -0700

    better answer

--- python/rna_transcription.py
+++ python/rna_transcription.py
@@ -1,12 +1,6 @@
 def to_rna(dna_strand):
     return_string = ""
+    dict = {"G":"C", "C":"G", "T":"A", "A":"U"}
     for letter in dna_strand:
-        if letter == "G":
-            return_string += "C"
-        elif letter == "C":
-            return_string += "G"
-        elif letter == "T":
-            return_string += "A"
-        elif letter == "A":
-            return_string += "U"
+        return_string += dict[letter]
     return return_string
