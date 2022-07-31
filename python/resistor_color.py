cbfac5567acc6ffd49464e301ff21382c3e22601
Andrew Klenner <atklenner@gmail.com>
2022-07-31 07:21:23 -0700

    answer

---
+++ python/resistor_color.py
@@ -0,0 +1,19 @@
+def color_code(color):
+    dict = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4, "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+    return dict[color]
+
+
+def colors():
+    expected = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
+    return expected
